ee6b6ee53155accee095737d7a4afbd611dbbb10
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-11-14 14:32:00 +0100

    add script

---
+++ home/bin/csv2json.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from os.path import basename
+import argparse
+import csv
+import json
+
+
+def rbac_csv2json(input_filename, output_filename):
+    with open(input_filename) as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+
+    with open(output_filename, 'w') as f:
+        json.dump(rows, f)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Convert CSV to JSON')
+
+    parser.add_argument('input_filename', type=str, help='CSV file name')
+
+    args = parser.parse_args()
+    filename = basename(args.input_filename)
+    output_filename = os.path.splitext(filename)[0] + ".json"
+
+    rbac_csv2json(args.input_filename, output_filename)
