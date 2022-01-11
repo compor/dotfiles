3e05eccef6cbda04f5ce8c85fab41ffce451cde9
Chris Vasiladiotis <cvassiladiotis@gmail.com>
2022-01-11 12:02:13 +0000

    Overhaul script for password migration to pass

--- home/bin/jpwsafe2pass.py
+++ home/bin/jpwsafe2pass.py
@@ -1,136 +1,174 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
+# Copyright (C) 2022 Chris Vasiladiotis <compor.dev@mailgw.com>. All Rights Reserved.
 # Copyright (C) 2013 Stefan Simroth <stefan.simroth@gmail.com>. All Rights Reserved.
 # Based on the script for KeepassX by Juhamatti Niemelä <iiska@iki.fi>.
 # This file is licensed under the GPLv2+. Please see COPYING for more information.
 #
-# Usage:
-# ./keepass2pass.py -f export.xml
-# By default, takes the name of the root element and puts all passwords in there, but you can disable this:
-# ./keepass2pass.py -f export.xml -r ""
-# Or you can use another root folder:
-# ./keepass2pass.py -f export.xml -r foo
-#
 # Features:
-# * This script can handle duplicates and will merge them.
-# * Besides the password also the fields 'UserName', 'URL' and 'Notes' (comment) will be inserted.
-# * You get a warning if an entry has no password, but it will still insert it.
+# * Besides the password also the fields 'username' and 'notes' (comment) will be inserted.
+# * A warning is issued if an entry has no password, but it will still insert it.
 
-import getopt, sys
+import argparse
+import sys
 from subprocess import Popen, PIPE
-from xml.etree import ElementTree
+from typing import List, Optional
+import xmltodict
+import logging
+import logging.config
+
+logger = logging.getLogger(__name__)
 
 
-def pass_import_entry(path, data):
-    """ Import new password entry to password-store using pass insert command """
-    proc = Popen(['pass', 'insert', '--multiline', path], stdin=PIPE, stdout=PIPE)
-    proc.communicate(data.encode('utf8'))
-    proc.wait()
-    
+def sanitize(s: str) -> str:
+    """ Sanitize string by using lowercase and replacing path unfriendly 
+    characters. """
+    return s.replace('\'', '').replace(' ', '-').lower()
 
-def get_value(elements, node_text):
-    for element in elements:
-        for child in element.findall('Key'):
-            if child.text == node_text:
-                return element.find('Value').text
-    return ''        
 
-def path_for(element, path=''):
-    """ Generate path name from elements title and current path """
-    if element.tag == 'Entry':
-        title = get_value(element.findall("String"), "Title")
-    elif element.tag == 'Group':
-        title = element.find('Name').text
-    else: title = ''
-    
-    if path == '': return title
-    else: return '/'.join([path, title])
+# TODO use a library for this
+def check_import_schema(doc) -> bool:
+    """ Check that the import dict has the expected schema. """
+    if 'pwlist' not in doc:
+        return False
 
-def password_data(element, path=''):
+    pwlist = doc['pwlist']
+
+    expected_fields = ['group', 'title', 'username', 'password', 'notes']
+
+    for pwentry in pwlist['pwentry']:
+        for f in expected_fields:
+            if f not in pwentry:
+                return False
+
+    return True
+
+
+def pass_import_entry(path: str, data: str, dry_run=True):
+    """ Import new password entry to password-store using pass insert command. """
+    cmd = ['pass', 'insert', '--multiline', path]
+    cmd_str = ' '.join(cmd)
+    logger.info(f'executing: {cmd_str}')
+    print(data)
+    print(data.encode('utf8'))
+
+    if not dry_run:
+        with Popen(cmd, stdin=PIPE, stdout=PIPE) as proc:
+            proc.communicate(data.encode('utf8'))
+
+
+def get_path_for(pwentry: dict):
+    """ Generate path name from entry group and entry title. """
+    group = pwentry['group']
+    title = pwentry['title']
+    path = '/'.join([group, title])
+    path = sanitize(path)
+
+    return path
+
+
+def get_data_for(pwentry: dict, fields=['username', 'notes']):
     """ Return password data and additional info if available from password entry element. """
-    data = ""
-    password = get_value(element.findall('String'), 'Password')
-    if password is not None: data = password + "\n"
+    data = ''
+    password = pwentry['password']
+
+    if password is not None:
+        data = password + '\n'
     else:
-        print "[WARN] No password: %s" % path_for(element, path)
-    
-    for field in ['UserName', 'URL', 'Notes']:
-        value = get_value(element, field)
-        if value is not None and not len(value) == 0:
-            data = "%s%s: %s\n" % (data, field, value)
+        path = get_path_for(pwentry)
+        logger.warning(f'No password for {path}')
+
+    for field in fields:
+        if field in pwentry:
+            value = pwentry[field]
+            if value is not None and not len(value) == 0:
+                data += field + ':' + value + '\n'
+        else:
+            logger.warning(f'No key for {field}')
+
     return data
 
-def import_entry(entries, element, path=''):
-    element_path = path_for(element, path) 
-    if entries.has_key(element_path):
-        print "[INFO] Duplicate needs merging: %s" % element_path
-        existing_data = entries[element_path]
-        data = "%s---------\nPassword: %s" % (existing_data, password_data(element))
-    else:
-        data = password_data(element, path)
-        
-    entries[element_path] = data
 
-def import_group(entries, element, path='', npath=None):
-    """ Import all entries and sub-groups from given group """
-    if npath is None:
-        npath = path_for(element, path)
-    for group in element.findall('Group'):
-        import_group(entries, group, npath)
-    for entry in element.findall('Entry'):
-        import_entry(entries, entry, npath)
-
-def import_passwords(xml_file, root_path=None):
-    """ Parse given Keepass2 XML file and import password groups from it """
-    print "[>>>>] Importing passwords from file %s" % xml_file
-    print "[INFO] Root path: %s" % root_path
+def import_entries(doc, groups: List[str] = None):
+    """ Parse given passwordsafe XML file and import password groups from it. """
     entries = dict()
-    with open(xml_file) as xml:
-        text = xml.read()
-        xml_tree = ElementTree.XML(text)
-        root = xml_tree.find('Root')
-        root_group = root.find('Group')
-        import_group(entries, root_group, '', root_path)
-        password_count = 0
-        for path, data in sorted(entries.iteritems()):
-            sys.stdout.write("[>>>>] Importing %s ... " % path.encode("utf-8"))
-            pass_import_entry(path, data)
-            sys.stdout.write("OK\n")
-            password_count += 1
-            
-        print "[ OK ] Done. Imported %i passwords." % password_count
+
+    pwlist = doc['pwlist']
+    entry_count = 0
+
+    for pwentry in pwlist['pwentry']:
+        group = pwentry['group']
+
+        if groups is not None and group not in groups:
+            continue
+
+        path = get_path_for(pwentry)
+        data = get_data_for(pwentry)
+
+        logger.info(f'[>>] Importing {path}...')
+
+        entries[path] = data
+
+        logger.info(f'[>>] Imported {path} successfully.')
+
+        entry_count += 1
+
+    logger.info(f'[ OK ] Done. Imported {entry_count} entries.')
+
+    return entries
 
 
-def usage():
-    """ Print usage """
-    print "Usage: %s -f XML_FILE" % (sys.argv[0])
-    print "Optional:"
-    print " -r ROOT_PATH    Different root path to use than the one in xml file, use \"\" for none"
+def pass_import_entries(imported_entries: dict, root_path: Optional[str] = None, dry_run=True):
+    """ Import password entries to pass. """
+    for k in imported_entries:
+        path = k if root_path is None else '/'.join([root_path, k])
+        pass_import_entry(path, imported_entries[k], dry_run)
 
 
-def main(argv):
-    try:
-        opts, args = getopt.gnu_getopt(argv, "f:r:")
-    except getopt.GetoptError as err:
-        print str(err)
-        usage()
-        sys.exit(2)
-    
-    xml_file = None
-    root_path = None
-    
-    for opt, arg in opts:
-        if opt in "-f":
-            xml_file = arg
-        if opt in "-r":
-            root_path = arg
-    
-    if xml_file is not None:
-        import_passwords(xml_file, root_path)
-    else:
-        usage()
-        sys.exit(2)
-    
+def main():
+    parser = argparse.ArgumentParser(
+        description='Imports password entries from pwsafe (Java version) to pass.',
+        epilog='''Copyright(C) 2022 Chris Vasiladiotis < compor.dev@mailgw.com >. All Rights Reserved.
+        Copyright(C) 2013 Stefan Simroth < stefan.simroth@gmail.com >. All Rights Reserved.
+        Based on the script for KeepassX by Juhamatti Niemelä < iiska@iki.fi >.
+        This file is licensed under the GPLv2+. Please see COPYING for more information.''')
+
+    parser.add_argument('--file', '-f', required=True,
+                        type=str, help='Input XML file with pwsafe entries.')
+    parser.add_argument('--root-path', '-r',
+                        type=str, help='Add root path on every imported entry.')
+    parser.add_argument('--groups', '-g', type=str, nargs='+',
+                        default=None, help='Import only these groups.')
+    parser.add_argument('--no-dry-run', '-n', dest='dry_run', default=True,
+                        action='store_false',
+                        help='Run without actually inserting entries to pass (default: true).')
+    arg_group = parser.add_mutually_exclusive_group()
+    arg_group.add_argument('--verbose', '-v', action='store_true',
+                           help='Print messages verbosely (default: false).')
+    arg_group.add_argument('--logger-config-file', '-l', type=str,
+                           help='Config file for logging settings.')
+    args = parser.parse_args()
+
+    if args.logger_config_file:
+        logging.config.fileConfig(
+            fname=args.logger_config_file, disable_existing_loggers=False)
+
+    if args.verbose:
+        logger.addHandler(logging.StreamHandler())
+        logger.setLevel(logging.DEBUG)
+
+    imported_entries = dict()
+
+    with open(args.file) as fd:
+        doc = xmltodict.parse(fd.read())
+        if not check_import_schema(doc):
+            logger.error('Imported entries do not follow expected schema.')
+            sys.exit(2)
+        imported_entries = import_entries(doc, args.groups)
+        print(imported_entries)
+        pass_import_entries(imported_entries, args.root_path, args.dry_run)
+
+
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
